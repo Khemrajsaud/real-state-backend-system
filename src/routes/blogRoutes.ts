4202611f0df8c5b6023aac1311a1cf61dd60942e
khemraj <khemrajsaud56@gmail.com>
2026-07-03 18:01:11 +0545

    blog route

---
+++ src/routes/blogRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import multer from "multer";
+import { createBlogPost, getAllBlogs, getBlogBySlug, deleteBlogPost } from "../controllers/blogController";
+
+const router = Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/", upload.single("coverImage"), createBlogPost);
+router.get("/", getAllBlogs);
+router.get("/:slug", getBlogBySlug);
+router.delete("/:id", deleteBlogPost);
+
+export default router;
